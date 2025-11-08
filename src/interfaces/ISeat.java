a5b6195535d65b424fa5eba2f37fa99fd65986eb
Arbin Isaac De La Torre - Rodriguez <delator5@purdue.edu>
2025-11-07 21:49:29 -0500

    Updated Comments for ISeat Interface

--- src/interfaces/ISeat.java
+++ src/interfaces/ISeat.java
@@ -4,9 +4,9 @@ public interface ISeat {
     int getRow();  //returns number of the row of the seat (0 = first row)
     int getNumber(); //returns the seat number within the row
     boolean isBooked();  //returns true if the seat is currently booked
-    void book(); //attempts to book the seat and returns true if successful
-    void cancel(); //attempts to cancel a booking and will returns true if successful
+    void book(); //Marks the seat as booked
+    void cancel(); //Frees the seat for booking
     double getPrice(); //returns the current price of the seat
     void setPrice(double p); //updates the price of the seat to the given value
-    String getSeatLabel();   //returns a readable label for the seat (A6 or B5)
+    String getSeatLabel(); //returns a readable label for the seat (A6 or B5)
 }
